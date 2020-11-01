ae178d867eca4c33f033f27b318fffbc17b0d3b8
Alesya Demeshko <demeshko.ab@phystech.edu>
2020-11-01 16:45:26 +0300

    add solutions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,16 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+  let activity = parseFloat(sampleActivity);
+
+  if (
+      typeof sampleActivity !== "string" || activity > 15 || activity < 1 || isNaN(activity)) {
+      return false;
+  };
+
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let res = Math.log(MODERN_ACTIVITY / activity) / k;
+  return Math.ceil(res);
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if ( Array.isArray(members) ) {
+    return members.filter( item => typeof item === 'string' ).map( item => item.trim().toUpperCase()[0] ).sort().join('');
+  } else {
+    return false;
+  }
 };
